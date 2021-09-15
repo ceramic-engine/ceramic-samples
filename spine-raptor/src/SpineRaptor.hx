bfe30eadde184c7b4a1e5a50675f8777b824b0a8
Jeremy Faivre <contact@jeremyfa.com>
2021-09-15 18:34:06 +0200

    Specify spine version in raptor sample

--- spine-raptor/src/SpineRaptor.hx
+++ spine-raptor/src/SpineRaptor.hx
@@ -2,6 +2,8 @@ package;
 
 import ceramic.Scene;
 import ceramic.Spine;
+import ceramic.Timer;
+import ceramic.Utils;
 import elements.Im;
 
 using ceramic.SpinePlugin;
@@ -16,6 +18,14 @@ class SpineRaptor extends Scene {
     var shouldLoop:Map<String,Bool> = [
         Spines.RAPTOR_PRO.WALK => true
     ];
+
+    var someText:String = 'Hello World';
+
+    var someText2:String = 'Youpi World';
+
+    var animationList2:Array<String>;
+
+    var animation2:String;
 
     override function preload() {
 
@@ -41,6 +51,9 @@ class SpineRaptor extends Scene {
             animationList.push(anim.name);
         animationList.sort(Reflect.compare);
 
+        animationList2 = ['youpi', 'bref'];
+        animation2 = 'bref';
+
     }
 
     override function update(delta:Float) {
@@ -52,6 +65,12 @@ class SpineRaptor extends Scene {
         if (Im.select('Animation', Im.string(spine.animation), animationList)) {
             spine.loop = shouldLoop.exists(spine.animation);
         }
+
+        Im.select('Animation', Im.string(animation2), animationList2);
+
+        // Im.editText(Im.string(someText));
+
+        // Im.editText(Im.string(someText2));
 
         Im.end();
 
