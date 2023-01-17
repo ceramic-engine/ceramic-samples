03d3faa52e0264ec75373867757c260696728604
Jeremy Faivre <contact@jeremyfa.com>
2023-01-17 23:14:38 +0100

    Cleanup and minor API fix in particles sample

--- particles/src/MainScene.hx
+++ particles/src/MainScene.hx
@@ -1,17 +1,11 @@
 package;
 
 import ceramic.Particles;
-import ceramic.Quad;
 import ceramic.Scene;
-import ceramic.Timer;
 
 class MainScene extends Scene {
 
-    var logo:Quad;
-
     override function preload() {
-
-        // Add any asset you want to load here
 
         assets.add(Images.CERAMIC_PARTICLE);
 
@@ -21,29 +15,8 @@ class MainScene extends Scene {
 
         var particles = new Particles(new CeramicEmitter(assets));
         particles.pos(width * 0.5, height * 0.5);
+        particles.autoEmit = true;
         add(particles);
-
-        particles.emitter.emitContinuously();
-
-    }
-
-    override function update(delta:Float) {
-
-        // Here, you can add code that will be executed at every frame
-
-    }
-
-    override function resize(width:Float, height:Float) {
-
-        // Called everytime the scene size has changed
-
-    }
-
-    override function destroy() {
-
-        // Perform any cleanup before final destroy
-
-        super.destroy();
 
     }
 
